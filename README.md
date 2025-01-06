5a9241891426f07e03bf7c88829ed9d2b5365f9d
Lucas Inacio <Lucas.Inacio@sita.aero>
2025-01-06 20:46:20 -0300

    Starts SecretManager Project with the PoC of how to use user secrets in a .NET 9 console app. Read README for instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SecretManager
 PoC of how to use user secrets in .NET 9 console app
+
+# How to Execute this PoC
+run these commands in powershell in .csproj file folder
+
+dotnet user-secrets init
+$sa_password = "[SA password here]"
+dotnet user-secrets set "ConnectionStrings:GameStoreContext" "Server=mieeesssServerAddress;Database=myDataBase;User Id=myUsername;Password=$sa_password;"
---
+++ SecretManager/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+
+// Access secrets
+string connectionString = new SecretManager().GetSecret("ConnectionStrings:MyDatabase");
+var appName = System.Configuration.ConfigurationManager.ConnectionStrings["MyDatabase1"].ConnectionString;
+
+Console.WriteLine(connectionString);
+Console.WriteLine(appName);
+
+
+public class SecretManager
+{
+    private readonly IConfiguration _configuration;
+
+    public SecretManager()
+    {
+        // Build the configuration to include user secrets
+        _configuration = new ConfigurationBuilder()
+            .AddUserSecrets<SecretManager>() // Use the class type for identifying secrets
+            .Build();
+    }
+
+    public string GetSecret(string key)
+    {
+        // Retrieve the secret by key
+        return _configuration[key];
+    }
+}
